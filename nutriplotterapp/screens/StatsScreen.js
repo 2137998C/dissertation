f6c495bf2cc6803e4de2f1bbec899863cc784744
Joe Kadi <joekadi@joes-mbp.home>
2019-01-05 23:37:58 +0000

    Profile page cleaned and stats displayed

--- nutriplotterapp/screens/StatsScreen.js
+++ nutriplotterapp/screens/StatsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -7,40 +7,38 @@ import {
   Text,
   TouchableOpacity,
   View,
-  TextInput,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+  TextInput
+} from "react-native";
+import { WebBrowser } from "expo";
 
-import { MonoText } from '../components/StyledText';
+import { MonoText } from "../components/StyledText";
 
 export default class StatsScreen extends React.Component {
   static navigationOptions = {
     //header: null,
-	title: 'User Stats',
+    title: "User Stats"
   };
-  
-  state = { name: '' }
+
+  state = { name: "" };
   onChangeText = name => this.setState({ name });
   render() {
     return (
       <View>
-	    <Image source={require('../assets/images/plate.png')}/>
-	    <Text style={styles.title}>Enter a food:</Text> 
+        <Image source={require("../assets/images/plate.png")} />
+        <Text style={styles.title}>Enter a food:</Text>
         <TextInput
-		  onChangeText={this.onChangeText}
+          onChangeText={this.onChangeText}
           style={styles.nameInput}
           placeHolder="Enter a food"
           value={this.state.name}
         />
-		<TouchableOpacity onPress={this.onPress}>
+        <TouchableOpacity onPress={this.onPress}>
           <Text style={styles.buttonText}>Search</Text>
         </TouchableOpacity>
       </View>
-	  );
-  };
-  
-  
-};
+    );
+  }
+}
 
 const offset = 24;
 const styles = StyleSheet.create({
@@ -48,22 +46,22 @@ const styles = StyleSheet.create({
     height: offset * 2,
     margin: offset,
     paddingHorizontal: offset,
-    borderColor: '#111111',
-    borderWidth: 1,
+    borderColor: "#111111",
+    borderWidth: 1
   },
   title: {
     marginTop: offset,
     marginLeft: offset,
-    fontSize: offset,
+    fontSize: offset
   },
   buttonText: {
     marginLeft: offset,
-    fontSize: offset,
+    fontSize: offset
   },
   image: {
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'contain'
-  },
+    resizeMode: "contain"
+  }
 });
